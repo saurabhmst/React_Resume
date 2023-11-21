0b71a4601da7b71d6130e925f73e5988b6fe110f
Your Name <you@example.com>
2023-11-21 05:30:17 +0530

    complete reasume using react

---
+++ src/component/Resume.js
@@ -0,0 +1,302 @@
+import React from 'react'
+
+
+const Resume = () => {
+    return (
+
+        <>
+
+
+            <div className="container-fluid">
+            <div className="row">
+                <div className="text-center nameSize h1 fs-3"> <span style={{ fontSize: "40px;" }}>S</span>AURABH KUMAR <span
+                    style={{ fontSize: "40px;" }}>M</span>ISHRA</div>
+            </div>
+            <div className="row">
+                <div className="col-3"></div>
+                <div className="col-6">
+                    <ul className="contact-link">
+                        <li><i className="fa fa-phone" aria-hidden="true">&nbsp;<span className="borderBottom">+91-9359257997</span></i></li>
+                        <li><i className="fa fa-google " aria-hidden="true">&nbsp; <span className="borderBottom">saurbhmishra@gmail.com</span></i></li>
+                        <li><i className="fa fa-linkedin " aria-hidden="true"></i> &nbsp; <span className="borderBottom">saurbhmishra</span></li>
+                        <li><i className="fa fa-github" aria-hidden="true"></i>&nbsp; <span className="borderBottom">saurabhmishra</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>EDUCATION</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <h5>Motilal Nehru National Institute Technology</h5>
+                </div>
+                <div className="col-5   text-end">
+                    <h5>12 2020 – 06 2023</h5>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <i> Master Of Computer Application - <b>8.7</b></i>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <h5>M.J.P Rohilkhand University</h5>
+                </div>
+                <div className="col-5 text-end">
+                    <h5>07 2016 – 10 2019</h5>
+                </div>
+            </div>
+            <div className="row " style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <i> Bachelor Of Science <b>6.5</b> cgpa</i>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <h5>P.C.S Inter College,Powayan</h5>
+                </div>
+                <div className="col-5   text-end">
+                    <h5>05 2013 – 06 2014</h5>
+                </div>
+            </div>
+            <div className="row " style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <i> ClassName XII <b>72%</b></i>
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>SKILLS</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-10">
+                    <ul className="contact-link-2">
+                        <li style={{ fontSize: "20px;" }}>Data Structures &<br />
+                            Algorithms</li>
+                        <li style={{ fontSize: "20px;" }}>Operating Systems</li>
+                        <li style={{ fontSize: "20px;" }}>Database Management
+                            <br />System (DBMS)</li>
+                        <li style={{ marginTop: "-18px;" }}>OOPS Concept</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>EXPERIENCE</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <h5><span className="borderBottom"> LGSI(SDE-Intern) </span></h5>
+                </div>
+                <div className="col-5   text-end">
+                    <h5>02/2023-05/2023</h5>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-10">
+                    <ul>
+                        <li>Developed robotics solution for the autonomous robot cleaner</li>
+                        <li>Resolved bugs and optimized code for smooth application performance</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>PROJECTS</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-1">
+                    <h5>  <span className="borderBottom"> LearningZone </span> </h5>
+                </div>
+                <div className="col-3" style={{ marginTop: "-18px;" }}>
+                    <p>|HTML, CSS, JavaScript, Django, SQLite</p>
+                </div>
+                <div className="col-6   text-end" style={{ marginTop: "-18px;" }}>
+                    <h5>02/2023-05/2023</h5>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-10">
+                    <ul>
+                        <li>LearningZone is a group project, it is a web application which gives a single dashboard to do activity related
+                            to study.</li>
+                        <li>Student can make todo list, add hoemwork, search on wikipedia , youtube and google books</li>
+                        <li>We have used HTML,CSS, Javascript, Django,SQLite.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-1">
+                    <h5> <span className="borderBottom"> Tic-Tac-Toe </span></h5>
+                </div>
+                <div className="col-3" style={{ marginTop: "-18px;" }}>
+                    <p>| Human vs Computer</p>
+                </div>
+                <div className="col-6   text-end" style={{ marginTop: "-18px;" }}>
+                    <h5>11/2021-12/2021</h5>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-10">
+                    <ul>
+                        <li>It uses Min-Max algorithm.</li>
+                        <li>Implemented using C++.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-1 ">
+                    <h5><span className="borderBottom">Book my court</span></h5>
+                </div>
+                <div className="col-3" style={{ marginTop: "-18px;" }}>
+                    <p>| webapp</p>
+                </div>
+                <div className="col-6 text-end" style={{ marginTop: "-18px;" }}>
+                    <h5>02/2022-04/2022</h5>
+                </div>
+                <div className="row" style={{ marginTop: "-18px;" }}>
+                    <div className="col-1"></div>
+                    <div className="col-10">
+                        <ul>
+                            <li>provide solution to book the court of my Institute</li>
+                            <li>Team project with the colloboration of different branches</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>TECHNICAL SKILLS</h4>
+                </div>
+            </div>
+            <div className="row" >
+                <div className="col-1"></div>
+                <div className="col-1">
+                    <h5>Languages:</h5>
+                </div>
+                <div className="col-10" style={{ marginTop: "-18px;" }}>
+                    C, C++,Python, Django, JavaScript,Java,SQL
+                </div>
+            </div>
+            <div className="row" >
+                <div className="col-1"></div>
+                <div className="col-2">
+                    <h5>Developer Tools:</h5>
+                </div>
+                <div className="col-9" style={{ marginTop: "-18px;" }}>
+                    VS Code,ecllips
+                </div>
+            </div>
+            <div className="row" >
+                <div className="col-1"></div>
+                <div className="col-2">
+                    <h5>Technologies/Frameworks:</h5>
+                </div>
+                <div className="col-9" style={{ marginLeft: "-40px;" }}>
+                    GitHub, Git,Django
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>EXTRACURRICULAR</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <h5>Member Of Application Club</h5>
+                </div>
+                <div className="col-5   text-end">
+                    <h5>10 2021 – 06 2023</h5>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <p><i>Teaching to Juniors</i></p>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <ul>
+                        <li>During My tensure at application club, i taught Oops to my Juniors</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 borderBottom">
+                    <h4>CERTIFICATIONS AND ACHIEVEMENTS</h4>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <ul>
+                        <li>3 star coder on codechef</li>
+                    </ul>
+                </div>
+                <div className="col-5 ">
+                    <ul>
+                        <li>Problem Solving Basic</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <ul>
+                        <li>C++ Certificate By MySirg</li>
+                    </ul>
+                </div>
+                <div className="col-5 ">
+                    <ul>
+                        <li>Solved 500 problems at various platforms</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row" style={{ marginTop: "-18px;" }}>
+                <div className="col-1"></div>
+                <div className="col-10 ">
+                    <ul>
+                        <li>Participated in Inter <b>NIT Coding Contest Codathon</b> organised by NIT Bhopal and secured 308 rank
+                            among more than 8k participants.</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+
+        </>
+
+    )
+}
+
+export default Resume
